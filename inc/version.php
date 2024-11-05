cd001d7ddb232cdd5ac3ae8047e592826dad5edb
John Elliot V <jj5@jj5.net>
2024-11-06 00:10:41 +1100

    Work, work...

--- inc/version.php
+++ inc/version.php
@@ -6,15 +6,16 @@
 define( 'KICKASS_CRYPTO_NAME', 'kickass-crypto' );
 define( 'KICKASS_CRYPTO_CODE', 'kickass-crypto' );
 
+//define( 'KICKASS_CRYPTO_VERSION', '0.2.3' );
 define( 'KICKASS_CRYPTO_VERSION_MAJOR', 0 );
 define( 'KICKASS_CRYPTO_VERSION_MINOR', 2 );
-define( 'KICKASS_CRYPTO_VERSION_PATCH', 1 );
+define( 'KICKASS_CRYPTO_VERSION_PATCH', 3 );
 
 define(
   'KICKASS_CRYPTO_SVN_DATE',
-  '$Date: 2024-11-05 23:57:11 +1100 (Tue, 05 Nov 2024) $'
+  '$Date: 2024-11-06 00:10:41 +1100 (Wed, 06 Nov 2024) $'
 );
-define( 'KICKASS_CRYPTO_SVN_REVISION', '$Revision: 1 $' );
+define( 'KICKASS_CRYPTO_SVN_REVISION', '$Revision: 127731528 $' );
 define( 'KICKASS_CRYPTO_SVN_AUTHOR', '$Author: jj5 $' );
 
-define( 'KICKASS_CRYPTO_GIT_DATE', '2024-11-05 23:57:11 +1100 (Tue, 05 Nov 2024)' );
+define( 'KICKASS_CRYPTO_GIT_DATE', '2024-11-06 00:10:41 +1100 (Wed, 06 Nov 2024)' );
